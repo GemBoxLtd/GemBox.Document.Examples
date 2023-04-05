47eba77fe91d34bbd8ae0660f95c8ba1046763b9
GemBox Dev Team <info@gemboxsoftware.com>
2023-04-05 11:14:26 +0200

    Updated "Charts" example.

--- C#/Basic Features/Charts/Program.cs
+++ C#/Basic Features/Charts/Program.cs
@@ -1,5 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using GemBox.Document;
+using GemBox.Pdf;
+using GemBox.Pdf.Content;
 using GemBox.Spreadsheet;
 using GemBox.Spreadsheet.Charts;
 
@@ -8,13 +14,18 @@ class Program
     static void Main()
     {
         // If using the Professional version, put your GemBox.Document serial key below.
-        ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+        GemBox.Document.ComponentInfo.SetLicense("FREE-LIMITED-KEY");
 
         // If using the Professional version, put your GemBox.Spreadsheet serial key below.
         SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
 
+        // If using the Professional version, put your GemBox.Pdf serial key below.
+        GemBox.Pdf.ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+
         Example1();
         Example2();
+        Example3();
+        Example4();
     }
 
     static void Example1()
@@ -66,10 +77,146 @@ class Program
         // Get underlying Excel chart and cast it as LineChart.
         var lineChart = (LineChart)chart.ExcelChart;
 
-        // Add new line series which has doubled values from the first series.
-        lineChart.Series.Add("Series 3", lineChart.Series.First()
-            .Values.Cast<double>().Select(val => val * 2));
+        // Get underlying Excel sheet and add new cell values.
+        var sheet = lineChart.Worksheet;
+        sheet.Cells["D1"].Value = "Series 3";
+        sheet.Cells["D2"].Value = 8.6;
+        sheet.Cells["D3"].Value = 5;
+        sheet.Cells["D4"].Value = 7;
+        sheet.Cells["D5"].Value = 9;
+
+        // Add new line series to the LineChart.
+        lineChart.Series.Add(sheet.Cells["D1"].StringValue, "Sheet1!D2:D5");
 
         document.Save("Updated Chart.docx");
+    }
+
+    static void Example3()
+    {
+        var document = new DocumentModel();
+
+        var chart = new Chart(document, GemBox.Document.ChartType.Column,
+            new FloatingLayout(
+                new HorizontalPosition(HorizontalPositionType.Center, HorizontalPositionAnchor.Margin),
+                new VerticalPosition(VerticalPositionType.Top, VerticalPositionAnchor.Paragraph),
+                new Size(10, 5, GemBox.Document.LengthUnit.Centimeter)));
+
+        document.Sections.Add(
+            new Section(document,
+                new Paragraph(document, chart)));
+
+        // Get underlying Excel chart.
+        var columnChart = (ColumnChart)chart.ExcelChart;
+
+        // Set chart's category labels from array.
+        columnChart.SetCategoryLabels(new string[] { "Columns 1", "Columns 2", "Columns 3" });
+
+        // Add chart's series from arrays.
+        columnChart.Series.Add("Values 1", new double[] { 3.4, 1.1, 3.7 });
+        columnChart.Series.Add("Values 2", new double[] { 4.4, 3.9, 3.5 });
+        columnChart.Series.Add("Values 3", new double[] { 2.9, 4.1, 1.9 });
+
+        document.Save("Created Chart from Array.docx");
+    }
+
+    static void Example4()
+    {
+        var document = DocumentModel.Load("Chart.docx");
+        var placeholdersMapping = ReplaceChartsWithPlaceholders(document);
+        document.Save("Chart.pdf");
+
+        using (var pdf = PdfDocument.Load("Chart.pdf"))
+        {
+            ReplacePlaceholdersWithCharts(pdf, placeholdersMapping);
+            pdf.Save();
+        }
+    }
+
+    static readonly string PlaceholderNameFormat = "GemBox_Chart_Placeholder_{0}";
+    static readonly Regex PlaceholderNameRegex = new Regex("GemBox_Chart_Placeholder_\\d+");
+    static readonly MemoryStream PlaceholderImage = new MemoryStream(File.ReadAllBytes("placeholder.png"));
+
+    static Dictionary<string, MemoryStream> ReplaceChartsWithPlaceholders(DocumentModel document)
+    {
+        var placeholdersMapping = new Dictionary<string, MemoryStream>();
+        int counter = 0;
+
+        foreach (Chart chart in document.GetChildElements(true, ElementType.Chart).Reverse())
+        {
+            // Replace Word chart with placeholder image that has specific title.
+            var placeholder = new Picture(document, PlaceholderImage, PictureFormat.Png, chart.Layout);
+            string placeholderName = string.Format(PlaceholderNameFormat, ++counter);
+            placeholder.Metadata.Title = placeholderName;
+            chart.Content.Start.InsertRange(placeholder.Content);
+            chart.Content.Delete();
+
+            // Retrieve Excel chart and export it as PDF.
+            var excelChart = (ExcelChart)chart.ExcelChart;
+            excelChart.Position.Width = chart.Layout.Size.Width;
+            excelChart.Position.Height = chart.Layout.Size.Height;
+            var chartAsPdfStream = new MemoryStream();
+            excelChart.Format().Save(chartAsPdfStream, GemBox.Spreadsheet.SaveOptions.PdfDefault);
+
+            // Map PDF that contains Excel chart to placeholder name.
+            placeholdersMapping.Add(placeholderName, chartAsPdfStream);
+        }
+
+        return placeholdersMapping;
+    }
+
+    static void ReplacePlaceholdersWithCharts(PdfDocument pdfDocument, Dictionary<string, MemoryStream> placeholdersMapping)
+    {
+        foreach (var page in pdfDocument.Pages)
+        {
+            // Find placeholders by searching for images with specific title.
+            var placeholders = FindPlaceholders(page);
+
+            foreach (var placeholder in placeholders)
+            {
+                if (!placeholdersMapping.TryGetValue(placeholder.Key, out MemoryStream chartAsPdfStream))
+                    continue;
+
+                PdfImageContent image = placeholder.Value.Item1;
+                PdfQuad bounds = placeholder.Value.Item2;
+
+                // Replace placeholder image with PDF that contains Excel chart.
+                using (var excelDocument = PdfDocument.Load(chartAsPdfStream))
+                {
+                    var form = excelDocument.Pages[0].ConvertToForm(pdfDocument);
+                    var formContentGroup = page.Content.Elements.AddGroup();
+                    var formContent = formContentGroup.Elements.AddForm(form);
+                    formContent.Transform = PdfMatrix.CreateTranslation(bounds.Left, bounds.Bottom);
+                }
+
+                image.Collection.Remove(image);
+            }
+        }
+    }
+
+    static Dictionary<string, Tuple<PdfImageContent, PdfQuad>> FindPlaceholders(PdfPage page)
+    {
+        var placeholders = new Dictionary<string, Tuple<PdfImageContent, PdfQuad>>();
+        var enumerator = page.Content.Elements.All(page.Transform).GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+            var element = enumerator.Current;
+            if (element.ElementType != PdfContentElementType.Image)
+                continue;
+
+            var imageElement = (PdfImageContent)element;
+            var metadata = imageElement.Image.Metadata?.Value;
+            if (metadata == null)
+                continue;
+
+            var match = PlaceholderNameRegex.Match(metadata);
+            if (!match.Success)
+                continue;
+
+            var bounds = imageElement.Bounds;
+            enumerator.Transform.Transform(ref bounds);
+            placeholders.Add(match.Value, Tuple.Create(imageElement, bounds));
+        }
+
+        return placeholders;
     }
 }
--- VB.NET/Basic Features/Charts/Program.vb
+++ VB.NET/Basic Features/Charts/Program.vb
@@ -1,5 +1,11 @@
+Imports System
+Imports System.Collections.Generic
+Imports System.IO
 Imports System.Linq
+Imports System.Text.RegularExpressions
 Imports GemBox.Document
+Imports GemBox.Pdf
+Imports GemBox.Pdf.Content
 Imports GemBox.Spreadsheet
 Imports GemBox.Spreadsheet.Charts
 
@@ -8,13 +14,18 @@ Module Program
     Sub Main()
 
         ' If using the Professional version, put your GemBox.Document serial key below.
-        ComponentInfo.SetLicense("FREE-LIMITED-KEY")
+        GemBox.Document.ComponentInfo.SetLicense("FREE-LIMITED-KEY")
 
         ' If using the Professional version, put your GemBox.Spreadsheet serial key below.
         SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY")
 
+        ' If using the Professional version, put your GemBox.Pdf serial key below.
+        GemBox.Pdf.ComponentInfo.SetLicense("FREE-LIMITED-KEY")
+
         Example1()
         Example2()
+        Example3()
+        Example4()
 
     End Sub
 
@@ -33,7 +44,7 @@ Module Program
                 New Paragraph(document, "New document with chart element."),
                 New Paragraph(document, chart)))
 
-        ' Get underlying Excel chart
+        ' Get underlying Excel chart.
         Dim excelChart As ExcelChart = DirectCast(chart.ExcelChart, ExcelChart)
         Dim worksheet As ExcelWorksheet = excelChart.Worksheet
 
@@ -65,11 +76,137 @@ Module Program
         ' Get underlying Excel chart and cast it as LineChart.
         Dim lineChart = DirectCast(chart.ExcelChart, LineChart)
 
-        ' Add new line series which has doubled values from the first series.
-        lineChart.Series.Add("Series 3", lineChart.Series.First() _
-            .Values.Cast(Of Double)().Select(Function(val) val * 2))
+        ' Get underlying Excel sheet and add new cell values.
+        Dim sheet = lineChart.Worksheet
+        sheet.Cells("D1").Value = "Series 3"
+        sheet.Cells("D2").Value = 8.6
+        sheet.Cells("D3").Value = 5
+        sheet.Cells("D4").Value = 7
+        sheet.Cells("D5").Value = 9
+
+        ' Add new line series to the LineChart.
+        lineChart.Series.Add(sheet.Cells("D1").StringValue, "Sheet1!D2:D5")
 
         document.Save("Updated Chart.docx")
     End Sub
 
+    Sub Example3()
+        Dim document As New DocumentModel()
+
+        Dim chart As New Chart(document, GemBox.Document.ChartType.Column,
+            New FloatingLayout(
+                New HorizontalPosition(HorizontalPositionType.Center, HorizontalPositionAnchor.Margin),
+                New VerticalPosition(VerticalPositionType.Top, VerticalPositionAnchor.Paragraph),
+                New Size(10, 5, GemBox.Document.LengthUnit.Centimeter)))
+
+        document.Sections.Add(
+            New Section(document,
+                New Paragraph(document, chart)))
+
+        ' Get underlying Excel chart.
+        Dim columnChart = DirectCast(chart.ExcelChart, ColumnChart)
+
+        ' Set chart's category labels from array.
+        columnChart.SetCategoryLabels(New String() {"Columns 1", "Columns 2", "Columns 3"})
+
+        ' Add chart's series from arrays.
+        columnChart.Series.Add("Values 1", New Double() {3.4, 1.1, 3.7})
+        columnChart.Series.Add("Values 2", New Double() {4.4, 3.9, 3.5})
+        columnChart.Series.Add("Values 3", New Double() {2.9, 4.1, 1.9})
+
+        document.Save("Created Chart from Array.docx")
+    End Sub
+
+    Sub Example4()
+        Dim document = DocumentModel.Load("Chart.docx")
+        Dim placeholdersMapping = ReplaceChartsWithPlaceholders(document)
+        document.Save("Chart.pdf")
+
+        Using pdf = PdfDocument.Load("Chart.pdf")
+            ReplacePlaceholdersWithCharts(pdf, placeholdersMapping)
+            pdf.Save()
+        End Using
+    End Sub
+
+    ReadOnly PlaceholderNameFormat As String = "GemBox_Chart_Placeholder_{0}"
+    ReadOnly PlaceholderNameRegex As Regex = New Regex("GemBox_Chart_Placeholder_\d+")
+    ReadOnly PlaceholderImage As MemoryStream = New MemoryStream(File.ReadAllBytes("placeholder.png"))
+
+    Function ReplaceChartsWithPlaceholders(document As DocumentModel) As Dictionary(Of String, MemoryStream)
+        Dim placeholdersMapping = New Dictionary(Of String, MemoryStream)()
+        Dim counter As Integer = 0
+
+        For Each chart As Chart In document.GetChildElements(True, ElementType.Chart).Reverse()
+            ' Replace Word chart with placeholder image that has specific title.
+            Dim placeholder = New Picture(document, PlaceholderImage, PictureFormat.Png, chart.Layout)
+            counter += 1
+            Dim placeholderName As String = String.Format(PlaceholderNameFormat, counter)
+            placeholder.Metadata.Title = placeholderName
+            chart.Content.Start.InsertRange(placeholder.Content)
+            chart.Content.Delete()
+
+            ' Retrieve Excel chart and export it as PDF.
+            Dim excelChart = CType(chart.ExcelChart, ExcelChart)
+            excelChart.Position.Width = chart.Layout.Size.Width
+            excelChart.Position.Height = chart.Layout.Size.Height
+            Dim chartAsPdfStream = New MemoryStream()
+            excelChart.Format().Save(chartAsPdfStream, GemBox.Spreadsheet.SaveOptions.PdfDefault)
+
+            ' Map PDF that contains Excel chart to placeholder name.
+            placeholdersMapping.Add(placeholderName, chartAsPdfStream)
+        Next
+
+        Return placeholdersMapping
+    End Function
+
+    Sub ReplacePlaceholdersWithCharts(pdfDocument As PdfDocument, placeholdersMapping As Dictionary(Of String, MemoryStream))
+        Dim chartAsPdfStream As MemoryStream = Nothing
+
+        For Each page In pdfDocument.Pages
+            ' Find placeholders by searching for images with specific title.
+            Dim placeholders = FindPlaceholders(page)
+
+            For Each placeholder In placeholders
+                If Not placeholdersMapping.TryGetValue(placeholder.Key, chartAsPdfStream) Then Continue For
+
+                Dim image As PdfImageContent = placeholder.Value.Item1
+                Dim bounds As PdfQuad = placeholder.Value.Item2
+
+                ' Replace placeholder image with PDF that contains Excel chart.
+                Using excelDocument = PdfDocument.Load(chartAsPdfStream)
+                    Dim form = excelDocument.Pages(0).ConvertToForm(pdfDocument)
+                    Dim formContentGroup = page.Content.Elements.AddGroup()
+                    Dim formContent = formContentGroup.Elements.AddForm(form)
+                    formContent.Transform = PdfMatrix.CreateTranslation(bounds.Left, bounds.Bottom)
+                End Using
+
+                image.Collection.Remove(image)
+            Next
+        Next
+    End Sub
+
+    Function FindPlaceholders(page As PdfPage) As Dictionary(Of String, Tuple(Of PdfImageContent, PdfQuad))
+        Dim placeholders = New Dictionary(Of String, Tuple(Of PdfImageContent, PdfQuad))()
+        Dim enumerator = page.Content.Elements.All(page.Transform).GetEnumerator()
+
+        While enumerator.MoveNext()
+
+            Dim element = enumerator.Current
+            If element.ElementType <> PdfContentElementType.Image Then Continue While
+
+            Dim imageElement = CType(element, PdfImageContent)
+            Dim metadata = imageElement.Image.Metadata?.Value
+            If metadata Is Nothing Then Continue While
+
+            Dim match = PlaceholderNameRegex.Match(metadata)
+            If Not match.Success Then Continue While
+
+            Dim bounds = imageElement.Bounds
+            enumerator.Transform.Transform(bounds)
+            placeholders.Add(match.Value, Tuple.Create(imageElement, bounds))
+
+        End While
+
+        Return placeholders
+    End Function
 End Module
